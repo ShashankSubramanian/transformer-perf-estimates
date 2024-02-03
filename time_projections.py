09d07401e157c9e7464491dc483d99295c8ca5f7
ermalrrapaj <ermalrrapaj@lbl.gov>
2024-02-02 17:01:59 -0800

    comm time and latency

--- time_projections.py
+++ time_projections.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+def comm_type_ops (n, comm_type):
+    match comm_type:
+        case 'allreduce':
+            result = 2*(n-1)/n
+        case 'reducescatter':
+            result = (n-1)/n
+        case 'allgather':
+            result = (n-1)/n
+        case 'broadcast':
+            result = 1
+        case 'reduce':
+            result 1
+
 
 def get_time_flops(flops, use_tensor=True, system={}):
     hardware_flops = system['matrix_flops_fp16'] if use_tensor else system['vector_flops_fp16']
@@ -17,12 +30,15 @@ def get_time_comm(vol, n_gpus=4, comm_type='allreduce', topology='nvlink', empir
         t_comm = 0
     else:
         if topology == 'nvlink': # use nvlink
-            t_comm  = vol / system['nvlink_bandwidth'] 
+            t_comm  = vol / system['nvlink_bandwidth'] * comm_type_ops(n,comm_type)
+            t_latency = np.log2(n_gpus) * system['nvlink_latency']
         elif topology == 'ib':
-            t_comm  = vol / system['ib_bandwidth'] 
+            t_comm  = vol / system['ib_bandwidth'] * comm_type_ops(n,comm_type)
+            t_latency = np.log2(n_gpus) * system['ib_latency']
         else:
             t_comm = 0
-    return t_comm * 10**3
+            t_latency =0
+    return (t_comm + t_latency)* 10**3
 
 def get_total_time(t_comp, t_comm, system={}, use_max=False):
     if use_max:
